89022ab0f8840cdfb5d31667e1f6ebf4da6c4f28
yuanmouren1hao <342834599@qq.com>
2015-05-02 15:09:45 +0800

    modify db config file

--- oa/inc/db.config.php
+++ oa/inc/db.config.php
@@ -1,7 +1,7 @@
 <?php
 	//数据库连接配置信息
 	$db_hoorayos_config = array(
-		'dsn'=>'mysql:host=localhost;dbname=oa',
+		'dsn'=>'mysql:host=localhost;dbname=test_blkq_db',
 		'name'=>'aoplee',
 		'password'=>'aoplee'
 	);

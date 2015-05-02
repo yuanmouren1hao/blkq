1fe84657fe81735b1bac1a02c8d9cee804d65ae7
yuanmouren1hao <342834599@qq.com>
2015-05-02 15:10:00 +0800

    modify setting.inc.php

--- oa/inc/setting.inc.php
+++ oa/inc/setting.inc.php
@@ -38,12 +38,9 @@ $uploadFileType = array(
 $uploadFileUnType = array('exe', 'cmd');
 //应用分类
 $apptype = array(
-	array('id'=>1,'name'=>'系统'),
-	array('id'=>2,'name'=>'游戏'),
-	array('id'=>3,'name'=>'影音'),
-	array('id'=>4,'name'=>'图书'),
-	array('id'=>5,'name'=>'生活'),
-	array('id'=>6,'name'=>'工具')
+	array('id'=>1,'name'=>'办公'),
+	array('id'=>2,'name'=>'工具'),
+	array('id'=>3,'name'=>'其他'),
 );
 //错误代码
 $errorcode = array(

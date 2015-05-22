6ddf49a1e98d2710e062d95503ad78c0c07f4cae
yuanmouren1hao <342834599@qq.com>
2015-05-22 10:07:15 +0800

    每个子类下面依然后很多文章，修改实现过程

--- home/Lib/Action/PageAction.class.php
+++ home/Lib/Action/PageAction.class.php
@@ -7,7 +7,7 @@ class PageAction extends Action
 	{
 
 		$this->display ();
-	
+
 	}
 
 	public function page_list()
@@ -19,7 +19,7 @@ class PageAction extends Action
 		{
 			$this->error ( "呃，发生了一点小错误了呢~~" );
 		}
-		
+
 		$obj = M ( 'page_catelog' );
 		import ( 'ORG.Util.Page' ); // 导入分页类
 		$count = $obj->where ( 'father="' . $father . '" and child="' . $child . '" and is_delt=0 ' )->count (); // 查询满足要求的总记录数
@@ -38,23 +38,33 @@ class PageAction extends Action
 
 		$child = $_REQUEST ['child'];
 		$father = $_REQUEST ['father'];
-		if ($child == null or $father == null)
+		$id = $_REQUEST['id'];
+
+		//dump($id);
+		if (($child == null || $father == null) && $id == null)
 		{
 			$this->error ( "呃，发生了一点小错误了呢~~" );
 		}
-		
-		$info = M ( "page_catelog" )->where ( "father='" . $father . "' and child='" . $child . "' and is_delt=0" )->order ( 'updatetime desc' )->limit ( 1 )->find ();
+
+		// get page info
+		if ($id == null){
+			$info = M ( "page_catelog" )->where ( "father='" . $father . "' and child='" . $child . "' and is_delt=0" )->order ( 'updatetime desc' )->limit ( 1 )->find ();
+		}else {
+			$info = M('page_catelog')->where('id = '.$id)->find();
+		}
+
+		//check info exist
 		if ($info == null)
 		{
 			$this->error ( "呃，该栏目下还没有文章呢，等待管理员添加哟~~" );
 		}
-		
+
 		$content = stripslashes ( $info ['content'] );
 		$info ['content'] = html_entity_decode ( $content );
 		$this->assign ( 'info', $info );
 		setInc ( 'page', $info ['id'], 'read_num', 1 );
 		$this->display ();
-	
+
 	}
 
 }
--- home/Tpl/Page/page_list.php
+++ home/Tpl/Page/page_list.php
@@ -4,25 +4,19 @@
 
 <div class="line padding-top padding-big-bottom">
 	<div class="x3 padding-big bg-gray-light border radius">
-	
 		<include file="Public/left" /> 
-		
 	</div>
+	
 	<div class="x9">
 		<include file="Public/bread" />
 		<div class="padding-large">
-			
-		
-		
 			<ul class="list-text list-underline list-striped">
 				<volist name="list" id="vo">
-				  <li><span class="date">{$vo.updatetime}</span><a href="<?php echo U("Page/page_detail");?>?id={$vo.id}">{$vo.title}</li>
+				  <li><span class="date">{$vo.updatetime}</span><a href="<?php echo U("Page/page_detail");?>?id={$vo.id}">{$vo.title}</a></li>
 			  	</volist>
 			</ul>
-			
-			
 			<div class="pages">
-				<?php echo $page_now?>
+				<?php echo $page_now;?>
 			</div>
 			
 		
